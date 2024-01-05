fb7969b4cfb4db1c45fe68f56bcd8c7e35640fb3
shm rsuf <shmusuf@gmail.com>
2024-01-05 15:47:32 +0600

    switch statement added

---
+++ switch.php
@@ -0,0 +1,45 @@
+<?php 
+
+// function colorName($color){
+//   switch ($color) {
+//     case 'red':
+//       echo "${color} is my favourite color \n";
+//       break;
+    
+//     case 'green':
+//       echo "${color} is my favourite color \n";
+//       break;
+  
+//     case 'yellow':
+//       echo "${color} is my favourite color \n";
+//       break;
+  
+//     case "black":
+//       echo "${color} is my favourite color \n";
+//       break;
+  
+//     case "violet":
+//       echo "${color} is my favourite color \n";
+//       break;
+    
+//     case "white":
+//       echo "${color} is my favourite color \n";
+//       break;
+
+//         default:
+//       echo "${color} is not any color name \n"; 
+//   }
+// }
+
+// colorName("black");
+// colorName("yellow");
+// colorName("green");
+// colorName("white");
+// colorName("violet");
+// colorName("fish");
+
+
+
+
+
+?>
