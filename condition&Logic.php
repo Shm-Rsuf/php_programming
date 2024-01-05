c06852001a610fd0f233d85c6e2e78dc71152628
shm rsuf <shmusuf@gmail.com>
2024-01-05 11:51:40 +0600

    condition and nested if, else added

---
+++ condition&Logic.php
@@ -0,0 +1,56 @@
+<?php  
+/* 
+equal to operator = 5 == 5, true
+not equal to operator = 5 != 5, false
+greater than operator = 5 > 5, false
+less than operator = 5 < 5, false
+greather then or equal = 5 >= 5, true
+less than or equal = 5 <= 5, true
+
+*/
+// $number = 10;
+
+// if($number % 2 ==0)
+// {
+//   echo "{$number} is an even number";
+// }else
+// {
+//   echo "{$number} is an odd number";
+
+// }
+
+/* nested if, else in php */
+// $condition1 = true;
+// $condition2 = false;
+// $condition3 = false;
+
+// if($condition1){
+//   if($condition2){
+//     if($condition3){
+//       echo "Hello";
+//     }else{
+//       echo "No 1";
+//     }
+//   }else{
+//     echo "No 2";
+//   }
+// }else{
+//   echo "No 3";
+// }
+
+// echo "\n";
+
+// if($condition1 && $condition2 && $condition3){
+//   echo "Hello";
+// }else if($condition1 && $condition2){
+//   echo "No 1";
+// }else if($condition1){
+//   echo "No 2";
+// }else{
+//   echo "No 3";
+// }
+
+
+
+
+?>
