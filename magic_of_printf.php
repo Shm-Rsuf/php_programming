f95b1a3f43f24d9da854906d395386af6e90cbc9
shm rsuf <shmusuf@gmail.com>
2024-01-05 10:18:04 +0600

    magic of printf added

---
+++ magic_of_printf.php
@@ -0,0 +1,23 @@
+<?php 
+$fName = "Sir";
+$mName = "Isac";
+$lName = "Newton";
+
+// printf('His name is %2$s %1$s %3$s',$fName, $mName, $lName); //variable swaping printing
+// echo "\n";
+
+// printf("The binary equivalent of %d is %b \n",12, 12);
+// printf('The binary equivalent of %1$d is %1$b',5);
+// echo "\n";
+
+// $fNumber = 11.1264;
+// printf("%.2f",$fNumber);
+
+// $number1 = 123.567;
+// $number2 = 23.988;
+
+// printf("%06.2f \n",$number1);
+// printf("%08.2f",$number2);
+
+
+?>
