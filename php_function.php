8ee6da093ed570c8a89a12ff18ba8d9428ddf5b7
shm rsuf <shmusuf@gmail.com>
2024-01-06 22:59:39 +0600

    php function added

---
+++ php_function.php
@@ -0,0 +1,29 @@
+<?php 
+
+// function callByRef(&$value){
+//   return $value+=5;
+// }
+
+// $sum = 2;
+// callByRef($sum);
+// echo $sum;
+// echo PHP_EOL;
+
+/* Variable Number of Arguments */
+// function myFunction(...$x){
+//  $result = 0;
+//  for($i = 0; $i<count($x); $i++){
+//   $result+= $x[$i];
+//  }
+//  return $result;
+// }
+
+// $answer1 = myFunction(2, 4, 7, 3, 4);
+// $answer2 = myFunction(2, 4, 1, 3, 4);
+
+// echo "The answer is : {$answer1} \n";
+// echo "The answer is : {$answer2} \n";
+
+
+
+?>
