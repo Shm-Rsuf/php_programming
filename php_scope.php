f4f85e477dfdd873d45d05df1b2d51c15da45c04
shm rsuf <shmusuf@gmail.com>
2024-01-08 16:44:36 +0600

    php scope added

---
+++ php_scope.php
@@ -0,0 +1,20 @@
+<?php 
+$myName = "usuf ali";
+
+function printName(){
+  // global $myName; // not prefarable
+  // echo $GLOBALS["myName"];
+}
+
+// printName();
+
+function localScope(){
+  $myName = "shm rsuf";
+  echo "{$myName} \n";
+}
+
+localScope();
+echo $GLOBALS["myName"];
+
+
+?>
