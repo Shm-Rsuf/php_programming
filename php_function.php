546b0e5f8198fd068b97ba39b1486353e7ca9ff9
shm rsuf <shmusuf@gmail.com>
2024-01-08 15:46:05 +0600

    php function added

--- php_function.php
+++ php_function.php
@@ -29,6 +29,67 @@
 // }
 // echo addNumbers(1.2, 5.2);
 
+// function isEven($number)
+// {
+//   if($number % 2 == 0)
+//   {
+//     echo "{$number} is an even number! \n";
+//   }else
+//   {
+//     echo "{$number} is an odd number! \n";
+//   }
+  
+// }
+
+// isEven(15);
+// isEven(14);
+
+// function sum($a, $b, $c): int{
+//   return $a + $b + $c;
+// }
+
+// echo "The value is : ".sum(1, 4, 7);
+
+/* Accepting unlimited parameter in php */
+
+// function sum(...$numbers): float{
+//   $res = 0;
+//   for ($i=0; $i < count($numbers); $i++) { 
+//     $res += $numbers[$i];
+//   }
+//   return $res;
+// }
+
+// echo sum(3.5,5,6,7,9);
+
+/* recurtion and recursive function */
+
+// function printN($number){
+//   if($number > 5){
+//     return;
+//   }
+
+//   echo "{$number} \n";
+//   $number++;
+//   printN($number);
+// }
+
+// printN(1);
+
+// function print2($start, $end, $stepping = 1){
+//   if($start > $end){
+//     return;
+//   }
+//   echo $start;
+//   echo PHP_EOL;
+//   $start += $stepping;
+
+//   print2($start, $end, $stepping);
+
+// }
+
+// print2(10, 15, 3);
+
 
 
 ?>
