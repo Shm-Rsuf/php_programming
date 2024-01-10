a747daae0fdb5813134cf48fee07c6e8120e0678
shm rsuf <shmusuf@gmail.com>
2024-01-10 10:08:03 +0600

    remove data from associative array

---
+++ rmv_data_f_ass_arr.php
@@ -0,0 +1,40 @@
+<?php 
+// $person = [
+//   "fName"=>"John",
+//   "lName"=>"Doe",
+//   "section"=>"B"
+// ];
+
+// unset function removes a value and its index also
+
+// print_r($person);
+// unset($person['section']);
+// print_r($person);
+
+// $numbers = [3,5,6,7,8];
+
+// unset($numbers[1]);
+// unset($numbers[3]);
+
+// print_r($numbers);
+
+/* To check the value is set or empty in php */
+// $name=0;
+
+// if(isset($name)){
+//   echo "Name set hoyese \n";
+// }else{
+//   echo "Name set hoyni \n";
+// }
+
+// if(empty($name)){
+//   echo "Name is empty \n";
+// }else{
+//   echo "Name is not empty \n";
+// }
+
+
+
+
+
+?>
