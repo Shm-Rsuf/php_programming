0c232c932736b27051c9b82f03ed853400fa15cf
shm rsuf <shmusuf@gmail.com>
2024-01-10 14:35:17 +0600

    array sort fn added

---
+++ sort_php.php
@@ -0,0 +1,25 @@
+<?php
+// $fruits = ["b" => "banana", "a" => "apple", "p" => "pinaple", "o" => "orange","m" => "mango"];
+
+// $numbers = [4,12, 22, 3, 5, 10];
+// $foods = ["Apple","Carot", "Banana", "apple", "banana", "carrot"];
+
+// krsort($fruits);
+
+// print_r($fruits);
+// echo "======== \n";
+
+// foreach ($fruits as $fruit) {
+//   echo $fruit . "\n";
+// }
+
+// krsort($numbers);
+// print_r($numbers);
+
+// asort($foods,2);
+// print_r($foods);
+
+
+
+
+?>
