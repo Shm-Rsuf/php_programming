4f64051ff8e6d869bbd0e1c2a6e13247adb16142
shm rsuf <shmusuf@gmail.com>
2024-01-10 10:43:38 +0600

    array_slice function added

---
+++ slice_fn_php.php
@@ -0,0 +1,31 @@
+<?php 
+$fruits = ["banana", "apple", "pinaple", "orange", "mango"];
+
+// $fruit = array_slice($fruits,-3,-1);
+// $fruit = array_slice($fruits,2, -1);
+// $fruit = array_slice($fruits,2, null,true);
+
+// print_r($fruit);
+// echo "\n";
+// print_r($fruits);
+
+// $random = [
+//   "a"=>12,
+//   "b"=>15,
+//   "c"=>18,
+//   "d"=>21,
+//   2=>21,
+//   "e"=>24,
+// ];
+
+// $randomData = array_slice($random, 2, null);
+// $randomData = array_slice($random, -5, 2);
+// $randomData = array_slice($random, -3, -1,true);
+//true mane jar ja index seivabei ashbe
+
+// print_r($randomData);
+// echo PHP_EOL;
+// print_r($random);
+
+
+?>
