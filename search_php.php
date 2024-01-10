5495a0578f0dc9e0b71d86b6b790351b2893e8cd
shm rsuf <shmusuf@gmail.com>
2024-01-10 16:39:13 +0600

    array search fn added

---
+++ search_php.php
@@ -0,0 +1,25 @@
+<?php 
+// $numbers = [42,78,"64",46];
+// $fruits = ["b" => "banana", "a" => "apple", "p" => "pinaple", "o" => "orange","m" => "mango"];
+
+// if(key_exists("p",$fruits)){
+//   echo "key is exist \n";
+// }else{
+//   echo "key is not exist \n";
+// }
+
+// To check element is exist or not
+// if(in_array(46,$numbers, true)){
+//   echo "match found \n";
+// }else{
+//   echo "match not found \n";
+// }
+
+// $isTrue = in_array(46,$numbers);
+// echo $isTrue;
+
+// $indexed = array_search(64,$numbers);
+// echo "index number of 64 is : ". $indexed . "\n";
+
+
+?>
