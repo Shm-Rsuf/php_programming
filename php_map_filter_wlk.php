18019ff7851d2c2edd839a42c319f7728e08529c
shm rsuf <shmusuf@gmail.com>
2024-01-10 16:56:43 +0600

    array filter, map and walk:(forEach in js) fn added

---
+++ php_map_filter_wlk.php
@@ -0,0 +1,24 @@
+<?php 
+// $numbers = array(1, 2, 3, 4, 5);
+
+// function isCondition($n){
+//   return $n >= 3;
+// }
+
+// $fArr = array_filter($numbers,'isCondition');
+// print_r($fArr);
+
+// function cube($number){
+//   printf("cube value of %d is : %d \n", $number, $number*$number*$number);
+// }
+
+// array_map('cube',$numbers);
+// print_r($numbers);
+
+// function printNumbers($number){
+//   printf("The value is : %d \n", $number);
+// }
+
+// array_walk($numbers,'printNumbers');
+
+?>
