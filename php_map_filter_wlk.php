60f6e272e4db6aa6f14f96503e2cab4b8fb8fa8f
shm rsuf <shmusuf@gmail.com>
2024-01-10 19:59:50 +0600

    array map, filter and walk added

--- php_map_filter_wlk.php
+++ php_map_filter_wlk.php
@@ -1,17 +1,27 @@
 <?php 
-$numbers = array(1, 2, 3, 4, 5, 6);
+// $numbers = array(1, 2, 3, 4, 5, 6, 7, 8);
+// $names = ["John", "Jane", "Bob", "Alice", "Alex", "Boby"];
 
-function isEven($n){
-  return $n % 2 == 0;
-}
+// function startWithJ($name){
+//   return $name[0] =="A";
+// }
+
+// $nameJ = array_filter($names,'startWithJ');
+// print_r($nameJ);
+
+// function isEven($n){
+//   return $n % 2 == 0;
+// }
 
 // function isOdd($n){
 //   return $n % 2 == 1;
 // }
 
-// $fArr = array_filter($numbers,'isEven');
-// $fArr = array_filter($numbers,'isOdd');
-// print_r($fArr);
+// $evenNumber = array_filter($numbers,'isEven');
+// $oddNumber = array_filter($numbers,'isOdd');
+
+// print_r($evenNumber);
+// print_r($oddNumber);
 
 // function cube($number){
 //   printf("cube value of %d is : %d \n", $number, $number*$number*$number);
