255c83dcf8680f2e293569a5f5c089dfc84eea75
shm rsuf <shmusuf@gmail.com>
2024-01-11 09:56:15 +0600

    list and range added

---
+++ range_list.php
@@ -0,0 +1,19 @@
+<?php 
+// list ==> list of multiple variables
+// $name = ["Arif", "Usuf","Jony","Momin"];
+// list($arif, $usuf, $jony) = $name;
+
+// printf("%s %s %s \n",$arif, $usuf, $jony);
+
+// $numbers = range(0, 20);
+// $len = count($numbers);
+
+// for ($i=0; $i < $len; $i++) {
+//   if($numbers[$i] > 0 && $numbers[$i] % 2 == 0){
+//     printf("numbers[%d] : %d \n",$i, $numbers[$i]);
+//   }
+// }
+
+
+
+?>
