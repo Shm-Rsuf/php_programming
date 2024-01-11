5c41b94686197e6c1e9353f6377b612fc5748f40
shm rsuf <shmusuf@gmail.com>
2024-01-11 10:44:23 +0600

    mt_rand & array_random added

---
+++ random_suffle.php
@@ -0,0 +1,37 @@
+<?php 
+// $numbers = range(20, 30);
+
+// $names = array(
+//   "p1" => "John",
+//   "p2" => "Jane",
+//   "p3" => "Alice",
+//   "p4" => "Bob",
+//   "p5" => "Eve"
+// );
+
+// array_rand returns associative array's key randomly
+// $key = array_rand($numbers);
+
+// echo $key . "\n";
+// echo $numbers[$key] . "\n";
+// print_r($numbers);
+
+// mt_rand fn generate a random number(min, max)
+
+// $random = mt_rand(0, count($numbers) - 1);
+// echo $random . "\n";
+// echo $numbers[$random] . "\n";
+
+// $luck = $numbers[$random];
+// if($luck % 2 == 0){
+//   echo "Head \n";
+// }else{
+//   echo "Tail \n";
+// }
+
+// shuffle($numbers);
+// print_r($numbers);
+
+
+
+?>
