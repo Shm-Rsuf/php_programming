b47af98e1040734125fa3f60570d0d718ee129db
shm rsuf <shmusuf@gmail.com>
2024-01-13 13:14:19 +0600

    php class initialize

---
+++ php_oop.php
@@ -0,0 +1,64 @@
+<?php 
+// class Human{
+//   function sayHi(){
+//     echo "Hello human\n";
+//   }
+// }
+
+// class Cat{
+//   function sayHi(){
+//     echo "Hello cat\n";
+//   }
+// }
+
+// $human1 = new Human();
+// $cat1 = new Cat();
+
+// $human1->sayHi();
+// $cat1->sayHi();
+
+// class Fruit{
+
+//   //properties
+//   public $name;
+//   public $color;
+
+//   //methods
+//   function set_name($name){
+//     $this->name = $name;
+//   }
+
+//   function get_name(){
+//    return $this->name;
+//   }
+
+//   function set_color($color){
+//     $this->color = $color;
+//   }
+
+//   function get_color(){
+//     return $this->color;
+//   }
+// }
+
+// $apple = new Fruit();
+// $apple->set_name("Apple");
+// $apple->set_color("red");
+
+// echo $apple->get_name() . "\n";
+// echo $apple->get_color() . "\n";
+
+// var_dump($apple);
+
+// class Fruit {
+//   public $name;
+// }
+// $apple = new Fruit();
+// $apple->name = "Apple";
+
+// echo $apple->name;
+
+
+
+
+?>
