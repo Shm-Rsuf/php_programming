f416712165fd4526c51ed3a534d2f68c2a458781
shm rsuf <shmusuf@gmail.com>
2024-01-16 10:45:18 +0600

    property overloading added

---
+++ property_overloading.php
@@ -0,0 +1,59 @@
+<?php 
+// class MotorCycle{
+//   private $parameters;
+
+//   function __construct($displacement, $mileage, $capacity)
+//   { $this->parameters = [];
+//     $this->parameters['displacement'] = $displacement;
+//     $this->parameters['mileage'] = $mileage;
+//     $this->parameters['capacity'] = $capacity;
+//   }
+
+  // function getDisplacement(){
+  //   echo $this->parameters['displacement'] . "\n";
+  // }
+
+  // function setDisplacement($displacement){
+  //   return $this->parameters['displacement'] = $displacement;
+  // }
+
+//   function __isset($name)
+//   {
+//     if(!isset($this->parameters[$name])){
+//       echo "{$name} not found";
+//       return false;
+//     }
+//     return true;
+//   }
+
+//   function __unset($name)
+//   {
+//     print_r($this->parameters);
+//     unset($this->parameters[$name]);
+//     print_r($this->parameters);
+//   }
+
+//   function __get($name)
+//   {
+//     echo $this->parameters[$name];
+//   }
+
+//   function __set($name, $value)
+//   {
+//     echo $this->parameters[$name] = $value;
+//   }
+
+// }
+
+// $pulser = new MotorCycle("150cc","45kmph","13ltr");
+
+
+// if(isset($pulser->tiresize)){
+//   echo $pulser->tiresize;
+// }
+
+// unset($pulser->mileage);
+
+
+
+?>
