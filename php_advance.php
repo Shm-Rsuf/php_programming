35941f236aa980218fc3cdbd62d5bb40f8a86184
shm rsuf <shmusuf@gmail.com>
2024-01-16 14:13:13 +0600

    fopen, fread, fclose fn added

---
+++ php_advance.php
@@ -0,0 +1,10 @@
+<?php 
+// $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+
+
+// echo fread($myfile,filesize("webdictionary.txt"));
+// echo fgets($myfile);
+// fclose($myfile);
+
+
+?>
