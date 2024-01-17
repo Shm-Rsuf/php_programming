7598c82058df6bd2bcc4a4e54c97c3d066423938
shm rsuf <shmusuf@gmail.com>
2024-01-17 12:45:26 +0600

    constant key added

---
+++ oop/constant.php
@@ -0,0 +1,16 @@
+<?php 
+// class GoodBye {
+//   const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+
+//   public function sayGoodBye(){
+//     echo self::LEAVING_MESSAGE . "\n";
+//   }
+// }
+
+// $goodBye = new GoodBye();
+// $goodBye->sayGoodBye();
+
+// echo GoodBye::LEAVING_MESSAGE; 
+//:: resolution operator
+
+?>
