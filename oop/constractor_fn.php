57da08c5659ead4309d66ab36b2d4ef2947f89b7
shm rsuf <shmusuf@gmail.com>
2024-01-17 10:44:25 +0600

    php constractor fn added

---
+++ oop/constractor_fn.php
@@ -0,0 +1,42 @@
+<?php 
+// class Person{
+//   public $name, $age;
+
+//   public function __construct($name, $age)
+//   {
+//     $this->name = $name;
+//     $this->age = $age;
+//   }
+
+//   function __set($name, $value)
+//   {
+//      $this->$name = $value;
+//   }
+
+
+//   function __get($name)
+//   {
+//     return $this->$name;
+//   }
+
+//   public function personInfo(){
+//     echo "Person name is {$this->name} and his/her age is {$this->age}\n";
+//   }
+
+// }
+
+// $usuf = new Person("usuf ali", 27);
+// $maruf = new Person("maruf", 25);
+
+// $usuf->name = "Usuf";
+// $maruf->name = "Arif";
+
+// $usuf->personInfo();
+// $maruf->personInfo();
+
+// echo $maruf->name . "\n";
+// echo $usuf->name . "\n";
+
+
+
+?>
