0619c2aac6092fc2e26adacdc150c3718cbf6048
shm rsuf <shmusuf@gmail.com>
2024-01-17 15:21:41 +0600

    abstract in php added

---
+++ oop/abstract.php
@@ -0,0 +1,32 @@
+<?php 
+// abstract class ParentClass {
+//   public $name;
+
+//   function __construct($name)
+//   {
+//     $this->name = $name;
+//   }
+
+//   abstract protected function info();
+// }
+
+// class ChildClass extends ParentClass{
+//   public function info(){
+//     echo "my name is {$this->name}\n";
+//   }
+// }
+
+// class ChildClass2 extends ParentClass{
+//   public function info(){
+//     echo "my name is {$this->name} and I am from Argentina\n";
+//   }
+// }
+
+// $c1 = new ChildClass("messi");
+// $c2 = new ChildClass2("messi");
+
+// $c1->info();
+// $c2->info();
+
+
+?>
