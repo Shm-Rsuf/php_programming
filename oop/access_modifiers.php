3660d408266df5724494c5d05c3cd4907be083eb
shm rsuf <shmusuf@gmail.com>
2024-01-17 11:39:13 +0600

    access_modifiers added

---
+++ oop/access_modifiers.php
@@ -0,0 +1,23 @@
+<?php
+// class Fruit {
+//   public $name;
+//   protected $color = '';
+//   private $weight = 0;
+
+//   function __set($name, $value)
+//   {
+//     $this->$name = $value;
+//   }
+
+//   function __get($name)
+//   {
+//     echo $this->$name;
+//   }
+// }
+
+// $mango = new Fruit();
+
+// echo $mango->name = 'Mango' . "\n"; // OK
+// echo $mango->color = 'Yellow' . "\n"; // OK
+// echo $mango->weight = '300' . "\n"; // OK
+?>
