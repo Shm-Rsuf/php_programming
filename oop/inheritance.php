53ec7b4098842fa3bb979fc82cabcad6b37ebfae
shm rsuf <shmusuf@gmail.com>
2024-01-17 11:11:07 +0600

    php inheritance added

---
+++ oop/inheritance.php
@@ -0,0 +1,41 @@
+<?php 
+// class Employee{
+//   public $name;
+//   public $age;
+//   public $salary;
+
+//   function __construct($n, $a, $s)
+//   {
+//     $this->name = $n;
+//     $this->age = $a;
+//     $this->salary = $s;
+//   }
+
+//   function info(){
+//     echo "Employee Profile :\n";
+//     echo "Name : {$this->name},\nAge : {$this->age},\nSalary : \${$this->salary}\n\n";
+//   }
+// }
+
+// class Manager extends Employee{
+//   public $ta = 250;
+//   public $phone = 50;
+//   public $total;
+
+//   function info(){
+//     $this->total = $this->ta + $this->phone + $this->salary;
+//     echo "Manager Profile :\n";
+//     echo "Name : {$this->name},\nAge : {$this->age},\nSalary : \${$this->total}\n\n";
+//   }
+// }
+
+// $jony = new Employee("Jony", 25, 2500);
+// $arif = new Manager("arif", 25, 10500);
+
+// $jony->info();
+// $arif->info();
+
+
+
+
+?>
