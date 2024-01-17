f09bd01b2c8145987ec3fbbef3da3db4cf17132a
shm rsuf <shmusuf@gmail.com>
2024-01-17 12:03:58 +0600

    property and method overriding added

---
+++ oop/overriding.php
@@ -0,0 +1,37 @@
+<?php
+// class Fruit {
+//   public $name;
+//   public $color;
+
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+
+//   public function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}.\n";
+//   }
+// }
+
+// class Strawberry extends Fruit {
+//   public $weight;
+
+//   public function __construct($name, $color, $weight) {
+//     $this->name = $name;
+//     $this->color = $color;
+//     $this->weight = $weight;
+//   }
+
+//   public function intro() {
+//     echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.\n";
+//   }
+// }
+
+// $apple = new Fruit("Apple", "red");
+// $strawberry = new Strawberry("Strawberry", "red",50);
+// $banana = new Strawberry("Banana", "yellow",150);
+
+// $apple->intro();
+// $strawberry->intro();
+// $banana->intro();
+?>
