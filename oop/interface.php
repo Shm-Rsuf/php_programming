3af70bb60fb1b9d3c6cb4cd48c29e8fe97acd360
shm rsuf <shmusuf@gmail.com>
2024-01-18 10:08:45 +0600

    interface implimentation added

---
+++ oop/interface.php
@@ -0,0 +1,41 @@
+<?php 
+// interface interfaceSum{
+//   function sum($a, $b);
+//   function mul($a, $b);
+//   function div($a, $b);
+// }
+
+// interface interfaceSub{
+//   function sub($a, $b);
+// }
+
+// class Calculation implements interfaceSum,interfaceSub{
+//   public function sum($a, $b){
+//     return $a + $b;
+//   }
+
+//   public function mul($a, $b){
+//     return $a * $b;
+//   }
+
+//   public function div($a, $b){
+//     return $a / $b;
+//   }
+
+//   public function sub($a, $b){
+//     return $a - $b;
+//   }
+// }
+
+// $c1 = new Calculation();
+
+// echo "sum of a + b : " . $c1->sum(25, 10) . "\n";
+// echo "sub of a - b : " . $c1->sub(25, 10) . "\n";
+// echo "multiplication of a * b : " . $c1->mul(25, 10) . "\n";
+// echo "division of a / b : " . $c1->div(25, 10) . "\n";
+
+
+
+
+
+?>
