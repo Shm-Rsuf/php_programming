8b524ea50a065c689272e1ab54b48953af49e949
shm rsuf <shmusuf@gmail.com>
2024-01-18 14:02:23 +0600

    php method chaining added

---
+++ oop/method_chaning.php
@@ -0,0 +1,25 @@
+<?php 
+// class base{
+
+//   public function first(){
+//     echo "I am from first function\n";
+//     return $this;
+//   }
+
+//   public function second(){
+//     echo "I am from second function\n";
+//     return $this;
+//   }
+
+//   public function third(){
+//     echo "I am from third function\n";
+//     return $this;
+//   }
+
+// }
+
+// $obj= new base();
+
+// $obj->first()->second()->third();
+
+?>
