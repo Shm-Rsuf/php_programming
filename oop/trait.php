95077663bfcc70ddb74004efff9db3d09a53683f
shm rsuf <shmusuf@gmail.com>
2024-01-18 10:50:14 +0600

    trait-can be used in multiple classes

---
+++ oop/trait.php
@@ -0,0 +1,65 @@
+<?php
+// trait message1{
+//   public function msg1(){
+//     echo "Hello, message1\n";
+//   }
+// }
+
+// trait message2{
+//   public function msg2(){
+//     echo "Hello, message2\n";
+//   }
+// }
+
+// class Class1{
+// use message1;
+
+// }
+
+// class Class2{
+//   use message1, message2;
+// }
+
+// $c1 = new Class1();
+// $c2 = new Class2();
+
+
+// $c1->msg1();
+// $c2->msg1();
+// $c2->msg2();
+
+// trait hello{
+//   public function sayHello(){
+//     echo "Hello everyone!\n";
+//   }
+
+//   public function sayHi(){
+//     echo "Hi everyone!\n";
+//   }
+// }
+
+// trait bye{
+//   public function sayBye(){
+//     echo "Bye Bye everyone!\n";
+//   }
+// }
+
+// class ClassHello{
+//   use hello,bye;
+// }
+
+// class ClassBye{
+//   use bye;
+// }
+
+// $hi = new ClassHello();
+
+// $hi->sayHello();
+// $hi->sayHi();
+// $hi->sayBye();
+
+
+
+
+
+?>
