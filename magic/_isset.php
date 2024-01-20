b31b60a2936304ec67d3ae1e81c8bc65fb6e765a
shm rsuf <shmusuf@gmail.com>
2024-01-20 15:10:51 +0600

    __isset fn added

--- magic/_isset.php
+++ magic/_isset.php
@@ -1,29 +1,27 @@
 <?php
 //বাইরে কোনো isset() ফাংশন কল হলে __isset()  ফাংশন অটোমেটিক্যালি কল হয়ে যাবে।
-class Student {
-  public $course;
-  private $fName;
-  private $lName;
+// class Student {
+//   public $course;
+//   public $fName;
+//   public $lName;
 
-  public function setName($fName, $lName){
-    $this->fName = $fName;
-    $this->lName = $lName;
-  }
+//   public function setName($fName, $lName){
+//     $this->fName = $fName;
+//     $this->lName = $lName;
+//   }
 
-  public function __isset($name)
-  {
-    echo isset($this->$name);
-  }
+//   public function __isset($name)
+//   {
+//     echo isset($this->$name);
+//   }
 
-}
+// }
 
-$s = new Student();
-$s->setName("usuf", "ali");
+// $s = new Student();
+// $s->setName("usuf", "ali");
 
 // echo isset($s->course);
-echo isset($s->fName);
-
-
+// echo isset($s->fName);
 
 
 ?>
