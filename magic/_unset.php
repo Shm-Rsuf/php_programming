b26ed73894bfe6f5d97020fc10f64bfe6436d443
shm rsuf <shmusuf@gmail.com>
2024-01-20 15:48:22 +0600

    __unset fn added

---
+++ magic/_unset.php
@@ -0,0 +1,30 @@
+<?php
+// class base {
+//   public $course = "php";
+
+//   private $fName;
+//   private $lName;
+
+//   public function setName($fName, $lName){
+//     $this->fName = $fName;
+//     $this->lName = $lName;
+//   }
+
+//   public function __unset($name)
+//   {
+//     unset($this->$name);
+//   }
+// }
+
+// $b = new base();
+
+// $b->setName("yaho", "baba");
+
+// unset($b->course);
+// echo $b->course;
+// print_r($b);
+
+// unset($b->fName);
+// print_r($b);
+
+?>
