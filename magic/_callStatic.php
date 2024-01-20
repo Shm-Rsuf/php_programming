d58a57b17cbcf9a1677d91d3f8cdb1a4d12ddcb5
shm rsuf <shmusuf@gmail.com>
2024-01-20 14:28:21 +0600

    __callStatic fn added

---
+++ magic/_callStatic.php
@@ -0,0 +1,35 @@
+<?php
+// class Math{
+// public static function __callStatic($method, $arguments)
+// {
+//   if("sum"=== $method){
+//     return array_sum($arguments);
+//   }
+//   echo "Static method $method does not exist";
+// }
+// }
+
+// // echo Math::sum(12, 3, 5);
+// echo Math::suma(12, 3, 5);
+
+// class Student {
+//   private static function hello(){
+//     echo "this is private static method\n";
+//   }
+
+//   public static function __callStatic($method, $arguments)
+//   {
+//     if(method_exists(__CLASS__, $method)){
+//       call_user_func_array([__CLASS__, $method], $arguments);
+//     }else{
+//       echo "Method ($method) does not exist\n";
+//     }
+//   }
+// }
+
+// Student::hello();
+// Student::hello2();
+
+
+
+?>
