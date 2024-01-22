4ff75bce38fe2a6f3e4cbd938b50f519eea627d0
shm rsuf <shmusuf@gmail.com>
2024-01-22 16:50:52 +0600

    php time fn added

---
+++ date_time/time1.php
@@ -0,0 +1,29 @@
+<?php
+// echo time() . "\n";
+// echo date("Y/m/d");
+
+
+// function factorial($n){
+//   $result = 1;
+//   if($n<=0) return $result;
+
+//   for ($i=1; $i <=$n ; $i++) { 
+//    $result *= $i;
+//   }
+
+//   return $result;
+// }
+
+// echo factorial(0) . "\n";
+// echo factorial(5) . "\n";
+
+// $startTime = microtime(true);
+// echo factorial(7) . "\n";
+// $endTime = microtime(true);
+// $excutionTime = $endTime - $startTime;
+
+// printf("%0.8f",$excutionTime);
+
+
+
+?>
